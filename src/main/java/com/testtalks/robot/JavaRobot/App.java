4cfe4f0622ea87a4d3b07bc9a60fb34a500ab5a2
diegomuner <diegomuner@hotmail.com>
2019-01-04 22:19:20 -0300

    1st commit - java+robot framework + selenium using MAVEN

---
+++ src/main/java/com/testtalks/robot/JavaRobot/App.java
@@ -0,0 +1,13 @@
+package com.testtalks.robot.JavaRobot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
